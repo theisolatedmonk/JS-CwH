ee219aafdf8ce76413d4e481252f52ca9a603f13
theisolatedmonk <96rahulseth@gmail.com>
2023-03-08 18:19:18 +0530

    Assignment_oprattors

---
+++ index.js
@@ -0,0 +1,66 @@
+ // 1.Ways to print in JavaScript
+ // console.log('Hello world');
+ // alert('alert me');
+ // document.write('This is document write');
+
+ // 2.JavaScript console API 
+ //  console.log('Hello world', 4 + 6, 'Another Log');
+ //  console.warn('this is worning');
+ //  console.error('This is an erro');
+
+ // 3. JavaScrip Variables
+ // what are variables? - Containers to store data values 
+ var number1 = 34;
+ var number2 = 56;
+ console.log(number1 + number2);
+ //  4. Data types in JavaScript
+ // Numbers
+ var num1 = 455;
+ var num2 = 56.76;
+ // String
+ var str1 = 'This is a string';
+ var str2 = "this is also a string";
+ // Objects
+ var marks = {
+         ravi: 34,
+         Shubham: 78,
+         Harry: 90.91
+     }
+     //  booleans
+ var a = true;
+ var b = false;
+ //  console.log(a, b);
+
+ var und = undefined;
+ var und;
+ //  console.log(und);
+
+ var n = null;
+ //  console.log(n);
+ /*
+  At a very high level, there are two types of data types in JavaScript
+  1. Primitive data type: undefined, null, number, string, boolean, symbol
+  2. Reference data types: Arrays and Objects
+  */
+
+ var arr = [1, 2, 'bablu', 3, 4, 5]
+     //  console.log(arr);
+
+ //  Operators in JavaScript
+ // Arithmetic Oprators
+ var a = 100;
+ var b = 10;
+ console.log('The value of a + b is ', a + b);
+ console.log('The value of a - b is ', a - b);
+ console.log('The value of a * b is ', a * b);
+ console.log('The value of a / b is ', a / b);
+
+ //  Assignment Operators
+ var c = b;
+ //  c += 2;
+ //  c -= 2;
+ //  c *= 2;
+ //  c /= 2;
+ //  console.log(c);
+
+ // Comparision Oprators
